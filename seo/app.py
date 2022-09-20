57d3829a1a372a559489c5f900ab7a4c76166368
Seo Dongjun <ilkasd616@gmail.com>
2022-09-20 22:32:44 +0900

    합본

--- seo/app.py
+++ seo/app.py
@@ -34,8 +34,13 @@ def home():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-@app.route('/register')
+@app.route('/login')
 def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/register')
+def register():
     msg = request.args.get("msg")
     return render_template('register.html', msg=msg)
 
@@ -49,10 +54,31 @@ def sign_up():
     doc = {
         "username": username_receive,  # 아이디
         "password": password_hash,  # 비밀번호
-        "email": email_receive,  # 이메일
+        "mail": email_receive,  # 이메일
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
 @app.route('/sign_up/check_dup', methods=['POST'])
@@ -61,6 +87,25 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
+
+@app.route('/sign_up/mail', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+
+
+    try:
+
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+
+
+        userinfo = db.user.find_one({'username': payload['username']}, {'_username': 0})
+        return jsonify({'result': 'success', 'email': userinfo['mail']})
+    except jwt.ExpiredSignatureError:
+
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
 
