5282999cfbb864c555d1a1152f9fd490eee296f6
Bang <rlaqudrl831@naver.com>
2022-09-20 19:08:38 +0900

    app.py

--- Bang/app.py
+++ Bang/app.py
@@ -1,1 +1,50 @@
-print("hello")
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.utsxudu.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dball_in_run
+
+from datetime import datetime
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/writing_list", methods=["POST"])
+def save_writing():
+    mytitle_receive = request.form['mytitle_give']
+    place_receive = request.form['place_give']
+    content_receive = request.form['content_give']
+    channel_receive = request.form['channel_give']
+
+    file = request.files["file_give"]  # 파일 업로드
+
+    extension = file.filename.split('.')[-1]
+
+    today = datetime.now()
+    mytime = today.strftime("%Y-%m-%d-%H-%M-%S")
+
+    filename = f'file-{mytime}'
+
+    save_to = f'static/{filename}.{extension}'  # 파일 업로드
+    file.save(save_to)
+
+    doc = {
+        'mytitle':mytitle_receive,
+        'place':place_receive,
+        'content':content_receive,
+        'channel':channel_receive,
+        'file': f'{filename}.{extension}'
+    }
+
+    db.writing.insert_one(doc)
+
+    return jsonify({'msg':'작성 완료!'})
+
+@app.route("/writing_list", methods=["GET"])
+def give_writing():
+    return jsonify({'msg':'GET 연결 완료!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
